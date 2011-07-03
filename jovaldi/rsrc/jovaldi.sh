a7bb8128de165a1610c68dd75744bdd373ec9698
David A. Solin <david@joval.org>
2011-07-02 19:45:09 -0500

    Automated build on Unix.  Fixed line breaks.

--- jovaldi/rsrc/jovaldi.sh
+++ jovaldi/rsrc/jovaldi.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# Copyright (C) 2011 jOVAL.org.  All rights reserved.
+# Copyright (C) 2011 jOVAL.org.  All rights reserved.                                                 |
 # This software is licensed under the AGPL 3.0 license available at http://www.joval.org/agpl_v3.txt
 #
 if [ "x${JAVA_HOME}" == x ]; then
