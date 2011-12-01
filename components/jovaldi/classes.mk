bb70b8d2bb6f9c23db05c63b80c1b0f6664b296f
David A. Solin <david@joval.org>
2011-12-01 17:51:01 -0600

    Massive re-organization of files.

---
+++ components/jovaldi/classes.mk
@@ -0,0 +1,4 @@
+CLASSES=\
+	org.joval.oval.di.ExecutionState	\
+	org.joval.oval.di.IPluginContainer	\
+	org.joval.oval.di.Main
---
+++ components/jovaldi/rsrc/jovaldi.bat
@@ -0,0 +1,6 @@
+@REM Copyright (c) 2011 jOVAL.org.  All rights reserved.
+@REM This software is licensed under the AGPL 3.0 license available at http://www.joval.org/agpl_v3.txt
+@ECHO OFF
+
+IF NOT DEFINED JAVA_HOME SET JAVA_HOME=jre
+%JAVA_HOME%\bin\java -Xmx2048m -Djava.library.path=plugin\default\lib -cp lib\jovaldi.jar;lib\jOVALCore.jar;lib\oval-schema-5.10.jar;lib\cal10n-api-0.7.4.jar;lib\slf4j-api-1.6.2.jar;lib\slf4j-ext-1.6.2.jar;lib\slf4j-jdk14-1.6.2.jar org.joval.oval.di.Main %*
