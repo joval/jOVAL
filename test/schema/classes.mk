f920ee2ef4f67af925e7b5ef086c2d8c191bd19b
David A. Solin <david@joval.org>
2011-12-07 21:21:36 -0600

    Fix for build break on Linux.

--- test/schema/classes.mk
+++ test/schema/classes.mk
@@ -2,8 +2,8 @@ CLASSES=\
 	org.joval.test.automation.schema.ObjectFactory	\
 	org.joval.test.automation.schema.package-info	\
 	org.joval.test.automation.schema.Report	\
-	org.joval.test.automation.schema.ResultEnumeration	\
 	org.joval.test.automation.schema.TestDocument	\
 	org.joval.test.automation.schema.TestResult	\
+	org.joval.test.automation.schema.TestResultEnumeration	\
 	org.joval.test.automation.schema.TestResults	\
 	org.joval.test.automation.schema.TestSuite
