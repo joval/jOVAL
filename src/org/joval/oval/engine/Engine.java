joval/jOVAL

commit 74d387f5fc0e9475049d8898d6d9ef96f7216066

Per D. Haynes, ignore any state in the check="none exist" case.

src/org/joval/oval/engine/Engine.java
@@ -869,19 +869,16 @@ public class Engine implements IEngine {
 	    existence.addStatus(StatusEnumeration.NOT_COLLECTED);
 	    break;
 	}
-   
-	ResultEnumeration existenceResult = existence.getResult(testDefinition.getCheckExistence());
-	if (state == null) {
+
+	if (testDefinition.getCheck() == CheckEnumeration.NONE_EXIST) {
 	    //
-	    // DAS: Note, the NONE_EXIST check is deprecated as of 5.3, and will be eliminated in 6.0
+	    // Per D. Haynes, in this case, any state and/or check should be ignored.
+	    // Note that the NONE_EXIST check is deprecated as of 5.3, and will be eliminated in 6.0.
 	    //
-	    if (testDefinition.getCheck() == CheckEnumeration.NONE_EXIST) {
-		JOVALSystem.getLogger().warn(JOVALMsg.STATUS_CHECK_NONE_EXIST, testDefinition.getCheckExistence(), testId);
-		testResult.setResult(existence.getResult(ExistenceEnumeration.NONE_EXIST));
-	    } else {
-		testResult.setResult(existenceResult);
-	    }
+	    JOVALSystem.getLogger().warn(JOVALMsg.STATUS_CHECK_NONE_EXIST, testDefinition.getCheckExistence(), testId);
+	    testResult.setResult(existence.getResult(ExistenceEnumeration.NONE_EXIST));
 	} else {
+	    ResultEnumeration existenceResult = existence.getResult(testDefinition.getCheckExistence());
 	    switch(existenceResult) {
 	      case TRUE:
 		testResult.setResult(check.getResult(testDefinition.getCheck()));

commit 09fffb14f6eb157326e977ba2c43ed798579dd94

Fix engine regression introduced in commit 74d387f, to properly process null states.

src/org/joval/oval/engine/Engine.java
@@ -870,13 +870,24 @@ public class Engine implements IEngine {
 	    break;
 	}
 
+	//
+	// Note that the NONE_EXIST check is deprecated as of 5.3, and will be eliminated in 6.0.
+	// Per D. Haynes, in this case, any state and/or check should be ignored.
+	//
 	if (testDefinition.getCheck() == CheckEnumeration.NONE_EXIST) {
-	    //
-	    // Per D. Haynes, in this case, any state and/or check should be ignored.
-	    // Note that the NONE_EXIST check is deprecated as of 5.3, and will be eliminated in 6.0.
-	    //
 	    JOVALSystem.getLogger().warn(JOVALMsg.STATUS_CHECK_NONE_EXIST, testDefinition.getCheckExistence(), testId);
 	    testResult.setResult(existence.getResult(ExistenceEnumeration.NONE_EXIST));
+
+	//
+	// If there are no items matching the object, or if there is no state for the test, then the result of the test is
+	// simply the result of the existence check.
+	//
+	} else if (sc.getItemsByObjectId(objectId).size() == 0 || stateId == null) {
+	    testResult.setResult(existence.getResult(testDefinition.getCheckExistence()));
+
+	//
+	// If there are items matching the object, then check the existence check, then (if successful) the check.
+	//
 	} else {
 	    ResultEnumeration existenceResult = existence.getResult(testDefinition.getCheckExistence());
 	    switch(existenceResult) {