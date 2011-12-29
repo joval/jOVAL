8ea29c08782d884c3577b23cdcacfd9d96856b8a
David A. Solin <david@joval.org>
2011-12-29 15:56:24 -0600

    Added jovaldi variable Java heap size configuration for Linux.

--- components/jovaldi/rsrc/jovaldi.sh
+++ components/jovaldi/rsrc/jovaldi.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
-# Copyright (C) 2011 jOVAL.org.  All rights reserved.                                                 |
+# Copyright (C) 2011 jOVAL.org.  All rights reserved.
 # This software is licensed under the AGPL 3.0 license available at http://www.joval.org/agpl_v3.txt
 #
 if [ "x${JAVA_HOME}" == x ]; then
     export JAVA_HOME=./jre
 fi
-${JAVA_HOME}/bin/java -Xmx1024m -cp lib/jovaldi.jar:lib/oval-schema-5.10.1.jar:lib/jOVALCore.jar:lib/cal10n-api-0.7.4.jar:lib/slf4j-api-1.6.2.jar:lib/slf4j-ext-1.6.2.jar:lib/slf4j-jdk14-1.6.2.jar org.joval.oval.di.Main "$@"
+if [ `uname -p` == x86_64 ]; then
+    export JMEM=-Xmx2048m
+else
+    export JMEM=-Xmx1024m
+fi
+${JAVA_HOME}/bin/java ${JMEM} -cp lib/jovaldi.jar:lib/oval-schema-5.10.1.jar:lib/jOVALCore.jar:lib/cal10n-api-0.7.4.jar:lib/slf4j-api-1.6.2.jar:lib/slf4j-ext-1.6.2.jar:lib/slf4j-jdk14-1.6.2.jar org.joval.oval.di.Main "$@"
