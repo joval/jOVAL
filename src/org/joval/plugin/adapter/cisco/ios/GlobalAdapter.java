d468fdb239fb01eb43dfd27ab1266f1803e7cba1
David A. Solin <david@joval.org>
2012-01-19 11:52:13 -0600

    Fixes for proper function of IOS global_test.

--- src/org/joval/plugin/adapter/cisco/ios/GlobalAdapter.java
+++ src/org/joval/plugin/adapter/cisco/ios/GlobalAdapter.java
@@ -7,12 +7,14 @@ import java.util.Collection;
 import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.Vector;
+import java.util.regex.Pattern;
 import javax.xml.bind.JAXBElement;
 
 import oval.schemas.common.MessageType;
 import oval.schemas.common.MessageLevelEnumeration;
 import oval.schemas.common.OperationEnumeration;
 import oval.schemas.common.SimpleDatatypeEnumeration;
+import oval.schemas.definitions.core.EntityObjectStringType;
 import oval.schemas.definitions.ios.GlobalObject;
 import oval.schemas.systemcharacteristics.core.FlagEnumeration;
 import oval.schemas.systemcharacteristics.core.ItemType;
@@ -27,6 +29,7 @@ import org.joval.intf.plugin.IRequestContext;
 import org.joval.io.PerishableReader;
 import org.joval.oval.CollectionException;
 import org.joval.oval.OvalException;
+import org.joval.oval.ResolveException;
 import org.joval.util.JOVALMsg;
 import org.joval.util.JOVALSystem;
 import org.joval.util.SafeCLI;
@@ -59,36 +62,72 @@ public class GlobalAdapter implements IAdapter {
     public void disconnect() {
     }
 
+    private static final String BUILDING = "Building configuration...";
+    private static final String CURRENT = "Current configuration";
+
     public Collection<JAXBElement<? extends ItemType>> getItems(IRequestContext rc) throws CollectionException, OvalException {
-	Collection<JAXBElement<? extends ItemType>> items = new Vector<JAXBElement<? extends ItemType>>();
-	items.add(JOVALSystem.factories.sc.ios.createGlobalItem(getItem()));
-	return items;
-    }
+	GlobalObject gObj = (GlobalObject)rc.getObject();
+	EntityObjectStringType globalCommand = gObj.getGlobalCommand();
+	OperationEnumeration op = globalCommand.getOperation();
 
-    // Private
-
-    private GlobalItem getItem() throws CollectionException {
 	List<String> lines = null;
+	Collection<String> commands = new Vector<String>();
 	try {
 	    lines = session.getTechSupport().getData(ITechSupport.GLOBAL);
+
+	    if (globalCommand.isSetVarRef()) {
+		commands.addAll(rc.resolve(globalCommand.getVarRef()));
+	    } else {
+		commands.add((String)globalCommand.getValue());
+	    }
+	} catch (ResolveException e) {
+	    throw new OvalException(e);
 	} catch (NoSuchElementException e) {
 	    throw new CollectionException(e);
 	}
 
-	StringBuffer sb = new StringBuffer();
-	for (String line : lines) {
-	    if (sb.length() > 0) {
-		sb.append('\n');
+	Collection<GlobalItem> items = new Vector<GlobalItem>();
+	for (String command : commands) {
+	    for (String line : lines) {
+		if (isGlobalCommand(line)) {
+		    boolean add = false;
+
+		    switch(op) {
+		      case EQUALS:
+			add = line.equals(command);
+			break;
+
+		      case PATTERN_MATCH:
+			add = Pattern.matches(command, line);
+			break;
+
+		      default:
+			throw new OvalException(JOVALSystem.getMessage(JOVALMsg.ERROR_UNSUPPORTED_OPERATION, op));
+		    }
+
+		    if (add) {
+			GlobalItem item = JOVALSystem.factories.sc.ios.createGlobalItem();
+			EntityItemStringType globalCommandType = JOVALSystem.factories.sc.core.createEntityItemStringType();
+			globalCommandType.setValue(line);
+			item.setGlobalCommand(globalCommandType);
+			items.add(item);
+		    }
+		}
 	    }
-	    sb.append(line);
 	}
 
-	GlobalItem item = JOVALSystem.factories.sc.ios.createGlobalItem();
-	if (sb.length() > 0) {
-	    EntityItemStringType globalCommand = JOVALSystem.factories.sc.core.createEntityItemStringType();
-	    globalCommand.setValue(sb.toString());
-	    item.setGlobalCommand(globalCommand);
+	Collection<JAXBElement<? extends ItemType>> wrappedItems = new Vector<JAXBElement<? extends ItemType>>();
+	for (GlobalItem item : items) {
+	    wrappedItems.add(JOVALSystem.factories.sc.ios.createGlobalItem(item));
 	}
-	return item;
+	return wrappedItems;
+    }
+
+    private boolean isGlobalCommand(String line) {
+	return line.length() > 0 &&		// not an empty line
+	       !line.startsWith(BUILDING) &&	// not a remark
+	       !line.startsWith(CURRENT) &&	// not a remark
+	       !line.startsWith("!") &&		// not a comment
+	       !line.startsWith(" ");		// global context command
     }
 }
