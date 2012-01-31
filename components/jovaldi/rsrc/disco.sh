f6c1921dbe340cce96beacfbc14e7ec7d68325fe
David A. Solin <david@joval.org>
2012-01-31 11:33:37 -0600

    Removed unnecessary URLStreamHandler package property from disco.sh.

--- components/jovaldi/rsrc/disco.sh
+++ components/jovaldi/rsrc/disco.sh
@@ -10,4 +10,4 @@ if [ `uname -p` == x86_64 ]; then
 else
     export JMEM=-Xmx1024m
 fi
-${JAVA_HOME}/bin/java ${JMEM} -Djava.protocol.handler.pkgs=org.joval.protocol -cp lib/jovaldi.jar:lib/oval-schema-5.10.1.jar:lib/jOVALCore.jar:lib/cal10n-api-0.7.4.jar:lib/slf4j-api-1.6.2.jar:lib/slf4j-ext-1.6.2.jar:lib/slf4j-jdk14-1.6.2.jar:plugin/cisco/lib/jOVALPluginShared.jar:plugin/cisco/lib/jOVALPluginCisco.jar:plugin/cisco/lib/tftp.jar org.joval.plugin.CiscoPlugin "$@"
+${JAVA_HOME}/bin/java ${JMEM} -cp lib/jovaldi.jar:lib/oval-schema-5.10.1.jar:lib/jOVALCore.jar:lib/cal10n-api-0.7.4.jar:lib/slf4j-api-1.6.2.jar:lib/slf4j-ext-1.6.2.jar:lib/slf4j-jdk14-1.6.2.jar:plugin/cisco/lib/jOVALPluginShared.jar:plugin/cisco/lib/jOVALPluginCisco.jar:plugin/cisco/lib/tftp.jar org.joval.plugin.CiscoPlugin "$@"
