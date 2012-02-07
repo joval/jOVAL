80f6efc464b6608b23cc974208f172ca11bf4c76
David A. Solin <david@joval.org>
2012-02-06 22:12:25 -0600

    Always run Java from the disco/jovaldi script base directory.

--- components/jovaldi/rsrc/jovaldi.bat
+++ components/jovaldi/rsrc/jovaldi.bat
@@ -2,6 +2,7 @@
 @REM This software is licensed under the AGPL 3.0 license available at http://www.joval.org/agpl_v3.txt
 @ECHO OFF
 
+PUSHD "%~dp0"
 IF NOT DEFINED JAVA_HOME SET JAVA_HOME=jre
 IF %PROCESSOR_ARCHITECTURE% EQU x86 (
     SET JMEM=-Xmx1024m
@@ -9,3 +10,4 @@ IF %PROCESSOR_ARCHITECTURE% EQU x86 (
     SET JMEM=-Xmx2048m
 )
 %JAVA_HOME%\bin\java %JMEM% -Djava.library.path=plugin\default\lib -cp lib\jovaldi.jar;lib\jOVALCore.jar;lib\oval-schema-5.10.1.jar;lib\cal10n-api-0.7.4.jar;lib\slf4j-api-1.6.2.jar;lib\slf4j-ext-1.6.2.jar;lib\slf4j-jdk14-1.6.2.jar;lib\svrl.jar org.joval.oval.di.Main %*
+POPD
