bec3d630a20a546f9173551e703fd80accf8a89b
David A. Solin <david@joval.org>
2012-05-18 17:52:18 -0500

    Added missing copyright statement to the ocil.Checklist class file.

--- src/org/joval/ocil/Checklist.java
+++ src/org/joval/ocil/Checklist.java
@@ -1,3 +1,4 @@
+// Copyright (C) 2012 jOVAL.org.  All rights reserved.
 // This software is licensed under the AGPL 3.0 license available at http://www.joval.org/agpl_v3.txt
 
 package org.joval.ocil;
