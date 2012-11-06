d6dfb3a55dea8828c980fed2c81cd86421a6a517
David A. Solin <david@joval.org>
2012-11-06 10:03:39 -0600

    Improved implementation of legacy TextfilecontentAdapter, to match the legacy state/item definition (instead of the up-to-date item definition).

--- src/org/joval/scap/oval/adapter/independent/Textfilecontent54Adapter.java
+++ src/org/joval/scap/oval/adapter/independent/Textfilecontent54Adapter.java
@@ -36,14 +36,14 @@ import org.joval.util.JOVALMsg;
 import org.joval.util.StringTools;
 
 /**
- * Evaluates Textfilecontent54Test OVAL tests.
+ * Retrieves Textfilecontent54Test OVAL items.
  *
  * DAS: Specify a maximum file size supported
  *
  * @author David A. Solin
  * @version %I% %G%
  */
-public class Textfilecontent54Adapter extends TextfilecontentAdapter {
+public class Textfilecontent54Adapter extends BaseFileAdapter<TextfilecontentItem> {
     // Implement IAdapter
 
     public Collection<Class> init(IBaseSession session) {
@@ -57,154 +57,195 @@ public class Textfilecontent54Adapter extends TextfilecontentAdapter {
 
     // Protected
 
-    /**
-     * Parse the file as specified by the Object, and decorate the Item.
-     */
-    @Override
+    protected Class getItemClass() {
+	return TextfilecontentItem.class;
+    }
+
     protected Collection<TextfilecontentItem> getItems(ObjectType obj, ItemType base, IFile f, IRequestContext rc)
 		throws IOException, CollectException {
 
 	Collection<TextfilecontentItem> items = new HashSet<TextfilecontentItem>();
+	TextfilecontentItem baseItem = (TextfilecontentItem)base;
+	Textfilecontent54Object tfcObj = (Textfilecontent54Object)obj;
+	InputStream in = null;
+	try {
+	    int flags = 0;
+	    if (tfcObj.isSetBehaviors()) {
+		if (tfcObj.getBehaviors().getMultiline()) {
+		    flags |= Pattern.MULTILINE;
+		}
+		if (tfcObj.getBehaviors().getIgnoreCase()) {
+		    flags |= Pattern.CASE_INSENSITIVE;
+		}
+		if (tfcObj.getBehaviors().getSingleline()) {
+		    flags |= Pattern.DOTALL;
+		}
+	    } else {
+		flags = Pattern.MULTILINE;
+	    }
+	    Pattern pattern = Pattern.compile(StringTools.regexPerl2Java((String)tfcObj.getPattern().getValue()), flags);
 
-	TextfilecontentItem baseItem = null;
-	if (base instanceof TextfilecontentItem) {
-	    baseItem = (TextfilecontentItem)base;
+	    //
+	    // Read the whole file into a String buffer for searching
+	    //
+	    byte[] buff = new byte[256];
+	    int len = 0;
+	    StringBuffer sb = new StringBuffer();
+	    in = f.getInputStream();
+	    while ((len = in.read(buff)) > 0) {
+		sb.append(StringTools.toASCIICharArray(buff), 0, len);
+	    }
+	    String s = sb.toString();
+
+	    //
+	    // Find all the matching items
+	    //
+	    Collection<TextfilecontentItem> allItems = new Vector<TextfilecontentItem>();
+	    OperationEnumeration op = tfcObj.getPattern().getOperation();
+	    switch(op) {
+	      case PATTERN_MATCH:
+		allItems.addAll(getItems(pattern, baseItem, s));
+		break;
+
+	      default:
+		String msg = JOVALMsg.getMessage(JOVALMsg.ERROR_UNSUPPORTED_OPERATION, op);
+		throw new CollectException(msg, FlagEnumeration.NOT_COLLECTED);
+	    }
+
+	    //
+	    // Filter the matches by instance number
+	    //
+	    String instanceNum = (String)tfcObj.getInstance().getValue();
+	    op = tfcObj.getInstance().getOperation();
+	    switch(op) {
+	      case EQUALS:
+		for (TextfilecontentItem item : allItems) {
+		    if (((String)item.getInstance().getValue()).equals(instanceNum)) {
+			items.add(item);
+		    }
+		}
+		break;
+
+	      case LESS_THAN:
+		for (TextfilecontentItem item : allItems) {
+		    int inum = Integer.parseInt((String)item.getInstance().getValue());
+		    int comp = Integer.parseInt(instanceNum);
+		    if (inum < comp) {
+			items.add(item);
+		    }
+		}
+		break;
+
+	      case LESS_THAN_OR_EQUAL:
+		for (TextfilecontentItem item : allItems) {
+		    int inum = Integer.parseInt((String)item.getInstance().getValue());
+		    int comp = Integer.parseInt(instanceNum);
+		    if (inum <= comp) {
+			items.add(item);
+		    }
+		}
+		break;
+
+	      case GREATER_THAN:
+		for (TextfilecontentItem item : allItems) {
+		    int inum = Integer.parseInt((String)item.getInstance().getValue());
+		    int comp = Integer.parseInt(instanceNum);
+		    if (inum > comp) {
+			items.add(item);
+		    }
+		}
+		break;
+
+	      case GREATER_THAN_OR_EQUAL:
+		for (TextfilecontentItem item : allItems) {
+		    int inum = Integer.parseInt((String)item.getInstance().getValue());
+		    int comp = Integer.parseInt(instanceNum);
+		    if (inum >= comp) {
+			items.add(item);
+		    }
+		}
+		break;
+
+	      case PATTERN_MATCH: {
+		Pattern p = Pattern.compile(instanceNum);
+		for (TextfilecontentItem item : allItems) {
+		    if (p.matcher((String)item.getInstance().getValue()).find()) {
+			items.add(item);
+		    }
+		}
+		break;
+	      }
+
+	      default:
+		String msg = JOVALMsg.getMessage(JOVALMsg.ERROR_UNSUPPORTED_OPERATION, op);
+		throw new CollectException(msg, FlagEnumeration.NOT_COLLECTED);
+	    }
+	} catch (PatternSyntaxException e) {
+	    session.getLogger().warn(JOVALMsg.ERROR_PATTERN, e.getMessage());
+	    throw new IOException(e);
+	} catch (IllegalArgumentException e) {
+	    MessageType msg = Factories.common.createMessageType();
+	    msg.setLevel(MessageLevelEnumeration.ERROR);
+	    msg.setValue(e.getMessage());
+	    rc.addMessage(msg);
+	} finally {
+	    if (in != null) {
+		try {
+		    in.close();
+		} catch (IOException e) {
+		    session.getLogger().warn(JOVALMsg.ERROR_FILE_STREAM_CLOSE, f.toString());
+		}
+	    }
 	}
-	Textfilecontent54Object tfcObj = null;
-	if (obj instanceof Textfilecontent54Object) {
-	    tfcObj = (Textfilecontent54Object)obj;
-	}
+	return items;
+    }
 
-	if (baseItem != null && tfcObj != null) {
-	    InputStream in = null;
-	    try {
-		//
-		// Construct all the necessary patterns
-		//
-		int flags = 0;
-		if (tfcObj.isSetBehaviors()) {
-		    if (tfcObj.getBehaviors().getMultiline()) {
-			flags |= Pattern.MULTILINE;
-		    }
-		    if (tfcObj.getBehaviors().getIgnoreCase()) {
-			flags |= Pattern.CASE_INSENSITIVE;
-		    }
-		    if (tfcObj.getBehaviors().getSingleline()) {
-			flags |= Pattern.DOTALL;
-		    }
-		} else {
-		    flags = Pattern.MULTILINE;
-		}
-		Pattern pattern = Pattern.compile(StringTools.regexPerl2Java((String)tfcObj.getPattern().getValue()), flags);
+    // Private
 
-		//
-		// Read the whole file into a buffer to search for the pattern
-		//
-		byte[] buff = new byte[256];
-		int len = 0;
-		StringBuffer sb = new StringBuffer();
-		in = f.getInputStream();
-		while ((len = in.read(buff)) > 0) {
-		    sb.append(StringTools.toASCIICharArray(buff), 0, len);
-		}
-		String s = sb.toString();
+    /**
+     * Built items for all matches of p in s.
+     */
+    private Collection<TextfilecontentItem> getItems(Pattern p, TextfilecontentItem baseItem, String s) {
+	Matcher m = p.matcher(s);
+	Collection<TextfilecontentItem> items = new Vector<TextfilecontentItem>();
+	for (int instanceNum=1; m.find(); instanceNum++) {
+	    TextfilecontentItem item = Factories.sc.independent.createTextfilecontentItem();
+	    item.setPath(baseItem.getPath());
+	    item.setFilename(baseItem.getFilename());
+	    item.setFilepath(baseItem.getFilepath());
+	    item.setWindowsView(baseItem.getWindowsView());
 
-		Collection<TextfilecontentItem> allItems = new Vector<TextfilecontentItem>();
-		OperationEnumeration op = tfcObj.getPattern().getOperation();
-		switch(op) {
-		  case PATTERN_MATCH:
-		    allItems.addAll(getItems(pattern, baseItem, s));
-		    break;
+	    EntityItemStringType patternType = Factories.sc.core.createEntityItemStringType();
+	    patternType.setValue(p.toString());
+	    item.setPattern(patternType);
 
-		  default:
-		    String msg = JOVALMsg.getMessage(JOVALMsg.ERROR_UNSUPPORTED_OPERATION, op);
-		    throw new CollectException(msg, FlagEnumeration.NOT_COLLECTED);
-		}
+	    EntityItemIntType instanceType = Factories.sc.core.createEntityItemIntType();
+	    instanceType.setDatatype(SimpleDatatypeEnumeration.INT.value());
+	    instanceType.setValue(Integer.toString(instanceNum));
+	    item.setInstance(instanceType);
 
-		//
-		// Filter by instance numbers
-		//
-		String instanceNum = (String)tfcObj.getInstance().getValue();
-		op = tfcObj.getInstance().getOperation();
-		switch(op) {
-		  case EQUALS:
-		    for (TextfilecontentItem item : allItems) {
-			if (((String)item.getInstance().getValue()).equals(instanceNum)) {
-			    items.add(item);
-			}
-		    }
-		    break;
+	    EntityItemAnySimpleType textType = Factories.sc.core.createEntityItemAnySimpleType();
+	    String textValue = m.group();
+	    //
+	    // Ignore the trailing null match, if there is one.
+	    //
+	    if (textValue != null || items.size() == 0) {
+                textType.setValue(textValue);
+                item.setText(textType);
 
-		  case LESS_THAN:
-		    for (TextfilecontentItem item : allItems) {
-			int inum = Integer.parseInt((String)item.getInstance().getValue());
-			int comp = Integer.parseInt(instanceNum);
-			if (inum < comp) {
-			    items.add(item);
-			}
-		    }
-		    break;
+                int sCount = m.groupCount();
+                if (sCount > 0) {
+                    for (int sNum=1; sNum <= sCount; sNum++) {
+                        String group = m.group(sNum);
+                        if (group != null) {
+                            EntityItemAnySimpleType sType = Factories.sc.core.createEntityItemAnySimpleType();
+                            sType.setValue(group);
+                            item.getSubexpression().add(sType);
+                        }
+                    }
+                }
 
-		  case LESS_THAN_OR_EQUAL:
-		    for (TextfilecontentItem item : allItems) {
-			int inum = Integer.parseInt((String)item.getInstance().getValue());
-			int comp = Integer.parseInt(instanceNum);
-			if (inum <= comp) {
-			    items.add(item);
-			}
-		    }
-		    break;
-
-		  case GREATER_THAN:
-		    for (TextfilecontentItem item : allItems) {
-			int inum = Integer.parseInt((String)item.getInstance().getValue());
-			int comp = Integer.parseInt(instanceNum);
-			if (inum > comp) {
-			    items.add(item);
-			}
-		    }
-		    break;
-
-		  case GREATER_THAN_OR_EQUAL:
-		    for (TextfilecontentItem item : allItems) {
-			int inum = Integer.parseInt((String)item.getInstance().getValue());
-			int comp = Integer.parseInt(instanceNum);
-			if (inum >= comp) {
-			    items.add(item);
-			}
-		    }
-		    break;
-
-		  case PATTERN_MATCH: {
-		    Pattern p = Pattern.compile(instanceNum);
-		    for (TextfilecontentItem item : allItems) {
-			if (p.matcher((String)item.getInstance().getValue()).find()) {
-			    items.add(item);
-			}
-		    }
-		    break;
-		  }
-
-		  default:
-		    String msg = JOVALMsg.getMessage(JOVALMsg.ERROR_UNSUPPORTED_OPERATION, op);
-		    throw new CollectException(msg, FlagEnumeration.NOT_COLLECTED);
-		}
-	    } catch (PatternSyntaxException e) {
-		session.getLogger().warn(JOVALMsg.ERROR_PATTERN, e.getMessage());
-		throw new IOException(e);
-	    } catch (IllegalArgumentException e) {
-		MessageType msg = Factories.common.createMessageType();
-		msg.setLevel(MessageLevelEnumeration.ERROR);
-		msg.setValue(e.getMessage());
-		rc.addMessage(msg);
-	    } finally {
-		if (in != null) {
-		    try {
-			in.close();
-		    } catch (IOException e) {
-			session.getLogger().warn(JOVALMsg.ERROR_FILE_STREAM_CLOSE, f.toString());
-		    }
-		}
+                items.add(item);
 	    }
 	}
 	return items;
--- src/org/joval/scap/oval/adapter/independent/TextfilecontentAdapter.java
+++ src/org/joval/scap/oval/adapter/independent/TextfilecontentAdapter.java
@@ -3,10 +3,13 @@
 
 package org.joval.scap.oval.adapter.independent;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Vector;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.regex.PatternSyntaxException;
@@ -46,7 +49,7 @@ public class TextfilecontentAdapter extends BaseFileAdapter<TextfilecontentItem>
     // Implement IAdapter
 
     public Collection<Class> init(IBaseSession session) {
-	Collection<Class> classes = new Vector<Class>();
+	Collection<Class> classes = new ArrayList<Class>();
 	if (session instanceof ISession) {
 	    super.init((ISession)session);
 	    classes.add(TextfilecontentObject.class);
@@ -66,108 +69,82 @@ public class TextfilecontentAdapter extends BaseFileAdapter<TextfilecontentItem>
     protected Collection<TextfilecontentItem> getItems(ObjectType obj, ItemType base, IFile f, IRequestContext rc)
 		throws IOException, CollectException {
 
-	Collection<TextfilecontentItem> items = new Vector<TextfilecontentItem>();
+	TextfilecontentItem baseItem = (TextfilecontentItem)base;
+	TextfilecontentObject tfcObj = (TextfilecontentObject)obj;
+	Collection<TextfilecontentItem> items = new ArrayList<TextfilecontentItem>();
+	BufferedReader reader = null;
+	try {
+	    //
+	    // Buffer the whole file into a List of lines
+	    //
+	    List<String> lines = new ArrayList<String>();
+	    reader = new BufferedReader(new InputStreamReader(f.getInputStream(), StringTools.ASCII));
+	    String line = null;
+	    while ((line = reader.readLine()) != null) {
+		lines.add(line);
+	    }
 
-	TextfilecontentItem baseItem = null;
-	if (base instanceof TextfilecontentItem) {
-	    baseItem = (TextfilecontentItem)base;
-	}
-	TextfilecontentObject tfcObj = null;
-	if (obj instanceof TextfilecontentObject) {
-	    tfcObj = (TextfilecontentObject)obj;
-	}
+	    OperationEnumeration op = tfcObj.getLine().getOperation();
+	    switch (op) {
+	      case PATTERN_MATCH: {
+		Pattern p = Pattern.compile(StringTools.regexPerl2Java((String)tfcObj.getLine().getValue()));
+		items.addAll(getItems(p, baseItem, lines));
+		break;
+	      }
 
-	if (baseItem != null && tfcObj != null) {
-	    InputStream in = null;
-	    try {
-		//
-		// Read the whole file into a buffer and search for the pattern
-		//
-		byte[] buff = new byte[256];
-		int len = 0;
-		StringBuffer sb = new StringBuffer();
-		in = f.getInputStream();
-		while ((len = in.read(buff)) > 0) {
-		    sb.append(StringTools.toASCIICharArray(buff), 0, len);
-		}
-		String s = sb.toString();
-
-		OperationEnumeration op = tfcObj.getLine().getOperation();
-		switch (op) {
-		  case PATTERN_MATCH: {
-		    Pattern p = Pattern.compile(StringTools.regexPerl2Java((String)tfcObj.getLine().getValue()));
-		    for (TextfilecontentItem item : getItems(p, baseItem, s)) {
-			EntityItemStringType lineType = Factories.sc.core.createEntityItemStringType();
-			lineType.setValue(tfcObj.getLine().getValue());
-			item.setLine(lineType);
-			items.add(item);
-		    }
-		    break;
-		  }
-
-		  default:
-		    String msg = JOVALMsg.getMessage(JOVALMsg.ERROR_UNSUPPORTED_OPERATION, op);
-		    throw new CollectException(msg, FlagEnumeration.NOT_COLLECTED);
-		}
-	    } catch (PatternSyntaxException e) {
-		session.getLogger().warn(JOVALMsg.ERROR_PATTERN, e.getMessage());
-		throw new IOException(e);
-	    } catch (IllegalArgumentException e) {
-		MessageType msg = Factories.common.createMessageType();
-		msg.setLevel(MessageLevelEnumeration.ERROR);
-		msg.setValue(e.getMessage());
-		rc.addMessage(msg);
-	    } finally {
-		if (in != null) {
-		    try {
-			in.close();
-		    } catch (IOException e) {
-			session.getLogger().warn(JOVALMsg.ERROR_FILE_STREAM_CLOSE, f.toString());
-		    }
+	      default:
+		String msg = JOVALMsg.getMessage(JOVALMsg.ERROR_UNSUPPORTED_OPERATION, op);
+		throw new CollectException(msg, FlagEnumeration.NOT_COLLECTED);
+	    }
+	} catch (PatternSyntaxException e) {
+	    session.getLogger().warn(JOVALMsg.ERROR_PATTERN, e.getMessage());
+	    throw new IOException(e);
+	} catch (IllegalArgumentException e) {
+	    MessageType msg = Factories.common.createMessageType();
+	    msg.setLevel(MessageLevelEnumeration.ERROR);
+	    msg.setValue(e.getMessage());
+	    rc.addMessage(msg);
+	} finally {
+	    if (reader != null) {
+		try {
+		    reader.close();
+		} catch (IOException e) {
+		    session.getLogger().warn(JOVALMsg.ERROR_FILE_STREAM_CLOSE, f.toString());
 		}
 	    }
 	}
 	return items;
     }
 
-    protected Collection<TextfilecontentItem> getItems(Pattern p, TextfilecontentItem baseItem, String s) {
-	Matcher m = p.matcher(s);
-	Collection<TextfilecontentItem> items = new Vector<TextfilecontentItem>();
-	for (int instanceNum=1; m.find(); instanceNum++) {
-	    TextfilecontentItem item = Factories.sc.independent.createTextfilecontentItem();
-	    item.setPath(baseItem.getPath());
-	    item.setFilename(baseItem.getFilename());
-	    item.setFilepath(baseItem.getFilepath());
-	    item.setWindowsView(baseItem.getWindowsView());
+    // Private
 
-	    EntityItemStringType patternType = Factories.sc.core.createEntityItemStringType();
-	    patternType.setValue(p.toString());
-	    item.setPattern(patternType);
+    /**
+     * Build all the item matches (old-style).
+     */
+    protected Collection<TextfilecontentItem> getItems(Pattern p, TextfilecontentItem baseItem, List<String> lines) {
+	Collection<TextfilecontentItem> items = new ArrayList<TextfilecontentItem>();
+	for (String line : lines) {
+	    EntityItemStringType lineType = Factories.sc.core.createEntityItemStringType();
+	    lineType.setValue(line);
 
-	    EntityItemIntType instanceType = Factories.sc.core.createEntityItemIntType();
-	    instanceType.setDatatype(SimpleDatatypeEnumeration.INT.value());
-	    instanceType.setValue(Integer.toString(instanceNum));
-	    item.setInstance(instanceType);
+	    Matcher m = p.matcher(line);
+	    if (m.find()) {
+		TextfilecontentItem item = Factories.sc.independent.createTextfilecontentItem();
+		item.setPath(baseItem.getPath());
+		item.setFilename(baseItem.getFilename());
+		item.setWindowsView(baseItem.getWindowsView());
+		item.setLine(lineType);
 
-	    EntityItemAnySimpleType textType = Factories.sc.core.createEntityItemAnySimpleType();
-	    String textValue = m.group();
-	    if (textValue != null || items.size() == 0) {
-                textType.setValue(textValue);
-                item.setText(textType);
-
-                int sCount = m.groupCount();
-                if (sCount > 0) {
-                    for (int sNum=1; sNum <= sCount; sNum++) {
-                        String group = m.group(sNum);
-                        if (group != null) {
-                            EntityItemAnySimpleType sType = Factories.sc.core.createEntityItemAnySimpleType();
-                            sType.setValue(group);
-                            item.getSubexpression().add(sType);
-                        }
-                    }
-                }
-
-                items.add(item);
+		int sCount = m.groupCount();
+		for (int sNum=0; sNum <= sCount; sNum++) {
+		    String group = m.group(sNum);
+		    if (group != null) {
+			EntityItemAnySimpleType sType = Factories.sc.core.createEntityItemAnySimpleType();
+			sType.setValue(group);
+			item.getSubexpression().add(sType);
+		    }
+		}
+		items.add(item);
 	    }
 	}
 	return items;
