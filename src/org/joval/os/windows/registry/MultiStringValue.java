09422a37ac0e95f74a56ab654997ad80704af398
David A. Solin <david@joval.org>
2012-12-12 11:02:23 -0600

    Handle REG_MULTI_SZ.toString when there the value is null (i.e., raw data is a single \0, not a double \0\0).

--- src/org/joval/os/windows/registry/MultiStringValue.java
+++ src/org/joval/os/windows/registry/MultiStringValue.java
@@ -27,14 +27,20 @@ public class MultiStringValue extends Value implements IMultiStringValue {
     }
 
     public String toString() {
-	StringBuffer sb = new StringBuffer("MultiStringValue [Name=\"").append(name).append("\" Value={");
-	for (int i=0; i < data.length; i++) {
-	    if (i > 0) {
-		sb.append(", ");
+	StringBuffer sb = new StringBuffer("MultiStringValue [Name=\"").append(name).append("\" Value=");
+	if (data == null) {
+	    sb.append("null");
+	} else {
+	    sb.append("{");
+	    for (int i=0; i < data.length; i++) {
+		if (i > 0) {
+		    sb.append(", ");
+		}
+		sb.append("\"").append(data[i]).append("\"");
 	    }
-	    sb.append("\"").append(data[i]).append("\"");
+	    sb.append("}");
 	}
-	sb.append("}]");
+	sb.append("]");
 	return sb.toString();
     }
 }
