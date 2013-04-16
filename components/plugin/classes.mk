c923f14621c3b72f1ed07d2c6367a792730c25d7
David A. Solin <david@joval.org>
2013-04-16 10:51:09 -0500

    Added support for dpkginfo_test.

--- components/plugin/classes.mk
+++ components/plugin/classes.mk
@@ -15,6 +15,7 @@ CLASSES=\
 	org.joval.scap.oval.adapter.independent.Textfilecontent54Adapter		\
 	org.joval.scap.oval.adapter.independent.TextfilecontentAdapter			\
 	org.joval.scap.oval.adapter.independent.XmlfilecontentAdapter			\
+	org.joval.scap.oval.adapter.linux.DpkginfoAdapter				\
 	org.joval.scap.oval.adapter.linux.PartitionAdapter				\
 	org.joval.scap.oval.adapter.linux.RpminfoAdapter				\
 	org.joval.scap.oval.adapter.linux.SelinuxbooleanAdapter				\
