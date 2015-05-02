eaa122ea1351bfc0c1eb578cbd91b445ceb794fa
David Solin <solind@Gallifrey.local>
2015-05-02 16:03:18 -0500

    1) Fixed schema catalogs for Cyberscope and DOD-ARF, to ref OVAL 5.11.1 schema definition files 2) Abbreviated package names for OVAL definitions and systemcharacteristics classes 3) Modified OVAL bindings so that ObjectType/id, ObjectType/version, StateType/id, StateType/version and ItemType/id are less likely to have overloaded subclasses.

--- scap-extensions/classes.mk
+++ scap-extensions/classes.mk
@@ -1,12 +1,12 @@
 CLASSES=\
-	scap.oval.definitions.linux.extended.AptBehaviors	\
-	scap.oval.definitions.linux.extended.AptObject	\
-	scap.oval.definitions.linux.extended.AptState	\
-	scap.oval.definitions.linux.extended.AptTest	\
-	scap.oval.definitions.linux.extended.EntityStateDpkgOperationType	\
-	scap.oval.definitions.linux.extended.ObjectFactory	\
-	scap.oval.definitions.linux.extended.package-info	\
-	scap.oval.systemcharacteristics.linux.extended.AptItem	\
-	scap.oval.systemcharacteristics.linux.extended.EntityItemDpkgOperationType	\
-	scap.oval.systemcharacteristics.linux.extended.ObjectFactory	\
-	scap.oval.systemcharacteristics.linux.extended.package-info
+	scap.oval.def.linux.extended.AptBehaviors	\
+	scap.oval.def.linux.extended.AptObject	\
+	scap.oval.def.linux.extended.AptState	\
+	scap.oval.def.linux.extended.AptTest	\
+	scap.oval.def.linux.extended.EntityStateDpkgOperationType	\
+	scap.oval.def.linux.extended.ObjectFactory	\
+	scap.oval.def.linux.extended.package-info	\
+	scap.oval.sc.linux.extended.AptItem	\
+	scap.oval.sc.linux.extended.EntityItemDpkgOperationType	\
+	scap.oval.sc.linux.extended.ObjectFactory	\
+	scap.oval.sc.linux.extended.package-info
