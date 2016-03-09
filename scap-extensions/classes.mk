78e73f6ab58d0349759f4d10f5628c8a31178bcd
David Solin <solind@Gallifrey.local>
2016-03-09 08:40:53 -0600

    Add x-win:junction_test as a schema extension.

--- scap-extensions/classes.mk
+++ scap-extensions/classes.mk
@@ -6,7 +6,15 @@ CLASSES=\
 	scap.oval.def.linux.extended.EntityStateDpkgOperationType	\
 	scap.oval.def.linux.extended.ObjectFactory	\
 	scap.oval.def.linux.extended.package-info	\
+	scap.oval.def.windows.extended.JunctionObject	\
+	scap.oval.def.windows.extended.JunctionState	\
+	scap.oval.def.windows.extended.JunctionTest	\
+	scap.oval.def.windows.extended.ObjectFactory	\
+	scap.oval.def.windows.extended.package-info	\
 	scap.oval.sc.linux.extended.AptItem	\
 	scap.oval.sc.linux.extended.EntityItemDpkgOperationType	\
 	scap.oval.sc.linux.extended.ObjectFactory	\
-	scap.oval.sc.linux.extended.package-info
+	scap.oval.sc.linux.extended.package-info	\
+	scap.oval.sc.windows.extended.JunctionItem	\
+	scap.oval.sc.windows.extended.ObjectFactory	\
+	scap.oval.sc.windows.extended.package-info
--- scap-extensions/schemas.mk
+++ scap-extensions/schemas.mk
@@ -1,3 +1,5 @@
 SCHEMAS=\
 	x-linux-definitions-schema.xsd			\
-	x-linux-system-characteristics-schema.xsd
+	x-linux-system-characteristics-schema.xsd	\
+	x-windows-definitions-schema.xsd		\
+	x-windows-system-characteristics-schema.xsd
