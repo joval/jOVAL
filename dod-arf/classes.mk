e1ed9e29eab1b9c4e8e7fc557f279a7475c315d1
David A. Solin <solin@farnamhallventures.com>
2019-05-28 11:09:25 -0500

    Cygwin/Windows build has errors when simultaneously making classes in the same package, so only specify the ObjectFactory and package-info classes in each package, to prevent this.

--- dod-arf/classes.mk
+++ dod-arf/classes.mk
@@ -1,272 +1,35 @@
 CLASSES=\
-	gov.nist.cve.feed.Nvd	\
 	gov.nist.cve.feed.ObjectFactory	\
 	gov.nist.cve.feed.package-info	\
-	mil.disa.arf.AssessmentReportType	\
-	mil.disa.arf.ContentType	\
 	mil.disa.arf.ObjectFactory	\
-	mil.disa.arf.OVALResultsType	\
 	mil.disa.arf.package-info	\
-	mil.disa.arf.ReportObjectType	\
-	mil.disa.arf.ScanDataIDType	\
-	mil.disa.arf.ScanDataType	\
-	mil.disa.arf.ScanLocationList	\
-	mil.disa.arf.ScannerType	\
-	mil.disa.arf.SCAPResultsType	\
-	mil.disa.arf.XCCDFResultsType	\
-	mil.disa.asset.ConfigurationType	\
-	mil.disa.asset.Country	\
-	mil.disa.asset.CountryType	\
-	mil.disa.asset.CPEInventoryType	\
-	mil.disa.asset.DeviceIdentifiersType	\
-	mil.disa.asset.DeviceIDType	\
-	mil.disa.asset.DeviceType	\
-	mil.disa.asset.DomainNameType	\
-	mil.disa.asset.ExternalNetworkDataIDType	\
-	mil.disa.asset.ExternalNetworkDataType	\
-	mil.disa.asset.FunctionType	\
-	mil.disa.asset.GeolocationIDType	\
-	mil.disa.asset.GeoLocationType	\
-	mil.disa.asset.GUIDType	\
-	mil.disa.asset.HostNetworkDataType	\
-	mil.disa.asset.LatitudeType	\
-	mil.disa.asset.LongitudeType	\
-	mil.disa.asset.NetworkConfigurationType	\
 	mil.disa.asset.ObjectFactory	\
 	mil.disa.asset.organization.ObjectFactory	\
-	mil.disa.asset.organization.OrganizationIDType	\
-	mil.disa.asset.organization.OrganizationType	\
 	mil.disa.asset.organization.package-info	\
-	mil.disa.asset.organization.PhoneType	\
 	mil.disa.asset.package-info	\
-	mil.disa.asset.person.GenerationalQualifierList	\
-	mil.disa.asset.person.GenerationalQualifierType	\
-	mil.disa.asset.person.NameType	\
 	mil.disa.asset.person.ObjectFactory	\
 	mil.disa.asset.person.package-info	\
-	mil.disa.asset.person.PersonIDType	\
-	mil.disa.asset.person.PersonType	\
-	mil.disa.asset.person.PhoneNumberType	\
-	mil.disa.asset.person.PhoneType	\
-	mil.disa.asset.RoleType	\
-	mil.disa.asset.StateType	\
-	mil.disa.cce.CceParameterType	\
-	mil.disa.cce.CceType	\
 	mil.disa.cce.ObjectFactory	\
 	mil.disa.cce.package-info	\
-	mil.disa.cpe.CheckType	\
-	mil.disa.cpe.CpeItem	\
-	mil.disa.cpe.CpeList	\
 	mil.disa.cpe.ObjectFactory	\
 	mil.disa.cpe.package-info	\
-	mil.disa.cpe.record.AssessedNameType	\
-	mil.disa.cpe.record.CpeRecordType	\
-	mil.disa.cpe.record.FamilyType	\
-	mil.disa.cpe.record.FunctionType	\
-	mil.disa.cpe.record.InstanceType	\
 	mil.disa.cpe.record.ObjectFactory	\
 	mil.disa.cpe.record.package-info	\
-	mil.disa.cpe.record.ParameterType	\
-	mil.disa.cpe.record.PatchType	\
-	mil.disa.cpe.record.PlatformNameType	\
-	mil.disa.cpe.record.PortRangeType	\
-	mil.disa.cpe.record.ProtocolAndPortRangeType	\
-	mil.disa.cpe.record.RoleType	\
-	mil.disa.cpe.record.ServiceType	\
-	mil.disa.cpe.record.SettingType	\
-	mil.disa.cpe.record.VulnerabilityType	\
-	mil.disa.cpe.ReferenceType	\
-	mil.disa.cve.CveStatus	\
-	mil.disa.cve.CveType	\
 	mil.disa.cve.ObjectFactory	\
 	mil.disa.cve.package-info	\
-	mil.disa.cvss.AccessComplexityEnumType	\
-	mil.disa.cvss.AccessComplexityType	\
-	mil.disa.cvss.AccessVectorEnumType	\
-	mil.disa.cvss.AccessVectorType	\
-	mil.disa.cvss.AuthenticationEnumType	\
-	mil.disa.cvss.AuthenticationType	\
-	mil.disa.cvss.BaseMetricsType	\
-	mil.disa.cvss.CiaEnumType	\
-	mil.disa.cvss.CiaRequirementEnumType	\
-	mil.disa.cvss.CiaRequirementType	\
-	mil.disa.cvss.CiaType	\
-	mil.disa.cvss.CollateralDamagePotentialEnumType	\
-	mil.disa.cvss.CollateralDamagePotentialType	\
-	mil.disa.cvss.ConfidenceEnumType	\
-	mil.disa.cvss.ConfidenceType	\
-	mil.disa.cvss.CvssImpactType	\
-	mil.disa.cvss.CvssType	\
-	mil.disa.cvss.EnvironmentalMetricsType	\
-	mil.disa.cvss.ExploitabilityEnumType	\
-	mil.disa.cvss.ExploitabilityType	\
-	mil.disa.cvss.MetricsType	\
 	mil.disa.cvss.ObjectFactory	\
 	mil.disa.cvss.package-info	\
-	mil.disa.cvss.RemediationLevelEnumType	\
-	mil.disa.cvss.RemediationLevelType	\
-	mil.disa.cvss.TargetDistributionEnumType	\
-	mil.disa.cvss.TargetDistributionType	\
-	mil.disa.cvss.TemporalMetricsType	\
 	mil.disa.patch.ObjectFactory	\
 	mil.disa.patch.package-info	\
-	mil.disa.patch.PatchType	\
-	mil.disa.scap.CheckReferenceType	\
-	mil.disa.scap.CheckSearchType	\
-	mil.disa.scap.NotesType	\
 	mil.disa.scap.ObjectFactory	\
 	mil.disa.scap.package-info	\
-	mil.disa.scap.ReferenceType	\
-	mil.disa.scap.SearchableCpeReferencesType	\
-	mil.disa.scap.TagType	\
-	mil.disa.scap.TextType	\
-	mil.disa.types.ActionType	\
-	mil.disa.types.AnyURIType	\
-	mil.disa.types.AuthorityInformationAccessType	\
-	mil.disa.types.AuthorityKeyIdentifierType	\
-	mil.disa.types.BooleanType	\
-	mil.disa.types.CertificatePolicyType	\
-	mil.disa.types.ClassificationType	\
-	mil.disa.types.ConfidentialityLevelList	\
-	mil.disa.types.ConfidentialityLevelType	\
-	mil.disa.types.CRLDistributionPointType	\
-	mil.disa.types.EntityIdentifierType	\
-	mil.disa.types.ExtensionsType	\
-	mil.disa.types.FIPS199ImpactLevelList	\
-	mil.disa.types.FIPS199ImpactLevelType	\
-	mil.disa.types.FunctionType	\
-	mil.disa.types.IPAddressType	\
-	mil.disa.types.KeyUsageType	\
-	mil.disa.types.MACLevelType	\
-	mil.disa.types.NameType	\
 	mil.disa.types.ObjectFactory	\
-	mil.disa.types.OperationalAttributeIDType	\
-	mil.disa.types.OperationalAttributesType	\
 	mil.disa.types.package-info	\
-	mil.disa.types.PhoneNumberType	\
-	mil.disa.types.PhoneType	\
-	mil.disa.types.PkiCertVersionList	\
-	mil.disa.types.RoleType	\
-	mil.disa.types.SecurityMarkingType	\
-	mil.disa.types.StatusEnum	\
-	mil.disa.types.StringType	\
-	mil.disa.types.SubjectKeyIdentifierType	\
-	mil.disa.types.SubjectPublicKeyInfoType	\
-	mil.disa.types.TaggedBooleanType	\
-	mil.disa.types.TaggeddateTimeType	\
-	mil.disa.types.TaggedDecimalType	\
-	mil.disa.types.TaggedHEXType	\
-	mil.disa.types.TaggedIntType	\
-	mil.disa.types.TaggedStringType	\
-	mil.disa.types.TaggedTokenType	\
-	mil.disa.types.TaggedURIType	\
-	mil.disa.types.ValidityType	\
-	mil.disa.types.X509CertType	\
-	mil.disa.vulnerability.AssociatedExploitLocationType	\
-	mil.disa.vulnerability.CweReferenceType	\
-	mil.disa.vulnerability.FixActionDescriptionEnumType	\
-	mil.disa.vulnerability.FixActionType	\
-	mil.disa.vulnerability.FixActionTypeEnumType	\
-	mil.disa.vulnerability.FixEffectivenessEnumType	\
 	mil.disa.vulnerability.ObjectFactory	\
-	mil.disa.vulnerability.OsvdbExtensionType	\
 	mil.disa.vulnerability.package-info	\
-	mil.disa.vulnerability.SecurityProtectionType	\
-	mil.disa.vulnerability.ToolConfigurationType	\
-	mil.disa.vulnerability.VulnerabilityReferenceCategoryEnumType	\
-	mil.disa.vulnerability.VulnerabilityReferenceType	\
-	mil.disa.vulnerability.VulnerabilityType	\
-	mil.disa.vulnerability.VulnerableSoftwareType	\
-	mil.disa.xccdf.Benchmark	\
-	mil.disa.xccdf.CcOperatorEnumType	\
-	mil.disa.xccdf.CheckContentRefType	\
-	mil.disa.xccdf.CheckContentType	\
-	mil.disa.xccdf.CheckExportType	\
-	mil.disa.xccdf.CheckImportType	\
-	mil.disa.xccdf.CheckType	\
-	mil.disa.xccdf.ComplexCheckType	\
-	mil.disa.xccdf.FactType	\
-	mil.disa.xccdf.FixStrategyEnumType	\
-	mil.disa.xccdf.FixTextType	\
-	mil.disa.xccdf.FixType	\
-	mil.disa.xccdf.GroupType	\
-	mil.disa.xccdf.HtmlTextType	\
-	mil.disa.xccdf.HtmlTextWithSubType	\
-	mil.disa.xccdf.IdentityType	\
-	mil.disa.xccdf.IdentType	\
-	mil.disa.xccdf.IdrefListType	\
-	mil.disa.xccdf.IdrefType	\
-	mil.disa.xccdf.InstanceFixType	\
-	mil.disa.xccdf.InstanceResultType	\
-	mil.disa.xccdf.InterfaceHintType	\
-	mil.disa.xccdf.ItemType	\
-	mil.disa.xccdf.MessageType	\
-	mil.disa.xccdf.MetadataType	\
-	mil.disa.xccdf.Model	\
-	mil.disa.xccdf.MsgSevEnumType	\
-	mil.disa.xccdf.NoticeType	\
 	mil.disa.xccdf.ObjectFactory	\
-	mil.disa.xccdf.OverrideableIdrefType	\
-	mil.disa.xccdf.OverrideableURIidrefType	\
-	mil.disa.xccdf.OverrideType	\
 	mil.disa.xccdf.package-info	\
-	mil.disa.xccdf.ParamType	\
-	mil.disa.xccdf.PlainTextType	\
-	mil.disa.xccdf.platform.Application	\
-	mil.disa.xccdf.platform.Hardware	\
-	mil.disa.xccdf.platform.LogicalOperator	\
 	mil.disa.xccdf.platform.ObjectFactory	\
-	mil.disa.xccdf.platform.Os	\
 	mil.disa.xccdf.platform.package-info	\
-	mil.disa.xccdf.platform.Platform	\
-	mil.disa.xccdf.platform.PlatformDefinitions	\
-	mil.disa.xccdf.platform.Product	\
-	mil.disa.xccdf.platform.ProductType	\
-	mil.disa.xccdf.platform.Service	\
-	mil.disa.xccdf.platform.TextType	\
-	mil.disa.xccdf.platform.VersionOperatorType	\
-	mil.disa.xccdf.ProfileNoteType	\
-	mil.disa.xccdf.ProfileRefineRuleType	\
-	mil.disa.xccdf.ProfileRefineValueType	\
-	mil.disa.xccdf.ProfileSelectType	\
-	mil.disa.xccdf.ProfileSetValueType	\
-	mil.disa.xccdf.ProfileType	\
-	mil.disa.xccdf.RatingEnumType	\
-	mil.disa.xccdf.ReferenceType	\
-	mil.disa.xccdf.ResultEnumType	\
-	mil.disa.xccdf.RoleEnumType	\
-	mil.disa.xccdf.RuleResultType	\
-	mil.disa.xccdf.RuleType	\
-	mil.disa.xccdf.ScoreType	\
-	mil.disa.xccdf.SelChoicesType	\
-	mil.disa.xccdf.SelectableItemType	\
-	mil.disa.xccdf.SelNumType	\
-	mil.disa.xccdf.SelStringType	\
-	mil.disa.xccdf.SeverityEnumType	\
-	mil.disa.xccdf.SignatureType	\
-	mil.disa.xccdf.Status	\
-	mil.disa.xccdf.StatusType	\
-	mil.disa.xccdf.TargetFactsType	\
-	mil.disa.xccdf.TestResultType	\
-	mil.disa.xccdf.TextType	\
-	mil.disa.xccdf.TextWithSubType	\
-	mil.disa.xccdf.URIidrefType	\
-	mil.disa.xccdf.UriRefType	\
-	mil.disa.xccdf.ValueOperatorType	\
-	mil.disa.xccdf.ValueType	\
-	mil.disa.xccdf.ValueTypeType	\
-	mil.disa.xccdf.VersionType	\
-	mil.disa.xccdf.WarningCategoryEnumType	\
-	mil.disa.xccdf.WarningType	\
-	mil.disa.xccdfp.CheckContentRefType	\
-	mil.disa.xccdfp.CheckContentType	\
-	mil.disa.xccdfp.CheckType	\
-	mil.disa.xccdfp.Fact	\
-	mil.disa.xccdfp.FactRefType	\
-	mil.disa.xccdfp.LogicOperatorEnumType	\
-	mil.disa.xccdfp.LogicTestType	\
 	mil.disa.xccdfp.ObjectFactory	\
-	mil.disa.xccdfp.package-info	\
-	mil.disa.xccdfp.Platform	\
-	mil.disa.xccdfp.PlatformSpecification	\
-	mil.disa.xccdfp.TextType
+	mil.disa.xccdfp.package-info
