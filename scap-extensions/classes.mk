ef2e598226fd88ffd5145f993d8102721eefc96a
David Solin <solin@farnamhallventures.com>
2020-05-02 16:55:06 -0500

    Experimental schema for Cisco NX-OS

--- scap-extensions/classes.mk
+++ scap-extensions/classes.mk
@@ -1,17 +1,101 @@
 CLASSES=\
+	scap.oval.sc.independent.extended.ShellcommandItem	\
 	scap.oval.sc.independent.extended.ObjectFactory	\
 	scap.oval.sc.independent.extended.package-info	\
+	scap.oval.sc.nexus.VersionItem	\
+	scap.oval.sc.nexus.SectionItem	\
+	scap.oval.sc.nexus.LineItem	\
+	scap.oval.sc.nexus.ObjectFactory	\
+	scap.oval.sc.nexus.GlobalItem	\
+	scap.oval.sc.nexus.package-info	\
+	scap.oval.sc.linux.extended.AptItem	\
 	scap.oval.sc.linux.extended.ObjectFactory	\
+	scap.oval.sc.linux.extended.EntityItemDpkgOperationType	\
 	scap.oval.sc.linux.extended.package-info	\
+	scap.oval.sc.windows.extended.CbscomponentapplicableItem	\
 	scap.oval.sc.windows.extended.ObjectFactory	\
 	scap.oval.sc.windows.extended.package-info	\
+	scap.oval.sc.protocols.EntityItemTlsCipherSuiteType	\
+	scap.oval.sc.protocols.EntityItemHttpMethodType	\
+	scap.oval.sc.protocols.EntityItemPublicKeyTypeType	\
+	scap.oval.sc.protocols.TlscertificateItem	\
+	scap.oval.sc.protocols.TlsciphersuiteItem	\
+	scap.oval.sc.protocols.EntityItemProtocolType	\
+	scap.oval.sc.protocols.FtpItem	\
+	scap.oval.sc.protocols.SmtpItem	\
+	scap.oval.sc.protocols.TlsversionItem	\
+	scap.oval.sc.protocols.EntityItemTlsSignatureSchemeType	\
 	scap.oval.sc.protocols.ObjectFactory	\
+	scap.oval.sc.protocols.HttpItem	\
+	scap.oval.sc.protocols.SshItem	\
+	scap.oval.sc.protocols.PortItem	\
+	scap.oval.sc.protocols.EntityItemTlsVersionType	\
 	scap.oval.sc.protocols.package-info	\
+	scap.oval.def.independent.extended.ShellcommandTest	\
+	scap.oval.def.independent.extended.ShellcommandObject	\
 	scap.oval.def.independent.extended.ObjectFactory	\
+	scap.oval.def.independent.extended.ShellcommandState	\
 	scap.oval.def.independent.extended.package-info	\
+	scap.oval.def.nexus.VersionState	\
+	scap.oval.def.nexus.GlobalObject	\
+	scap.oval.def.nexus.LineTest	\
+	scap.oval.def.nexus.GlobalState	\
+	scap.oval.def.nexus.SectionTest	\
+	scap.oval.def.nexus.VersionTest	\
+	scap.oval.def.nexus.SectionObject	\
+	scap.oval.def.nexus.ObjectFactory	\
+	scap.oval.def.nexus.GlobalTest	\
+	scap.oval.def.nexus.VersionObject	\
+	scap.oval.def.nexus.LineObject	\
+	scap.oval.def.nexus.LineState	\
+	scap.oval.def.nexus.SectionState	\
+	scap.oval.def.nexus.package-info	\
+	scap.oval.def.linux.extended.AptTest	\
+	scap.oval.def.linux.extended.AptState	\
 	scap.oval.def.linux.extended.ObjectFactory	\
+	scap.oval.def.linux.extended.AptObject	\
+	scap.oval.def.linux.extended.AptBehaviors	\
+	scap.oval.def.linux.extended.EntityStateDpkgOperationType	\
 	scap.oval.def.linux.extended.package-info	\
+	scap.oval.def.windows.extended.CbscomponentapplicableTest	\
+	scap.oval.def.windows.extended.CbscomponentapplicableObject	\
 	scap.oval.def.windows.extended.ObjectFactory	\
+	scap.oval.def.windows.extended.CbsComponentApplicableBehaviors	\
+	scap.oval.def.windows.extended.CbscomponentapplicableState	\
 	scap.oval.def.windows.extended.package-info	\
+	scap.oval.def.protocols.FtpTest	\
+	scap.oval.def.protocols.TlsciphersuiteTest	\
+	scap.oval.def.protocols.EntityObjectTlsSignatureSchemeType	\
+	scap.oval.def.protocols.TlsversionObject	\
+	scap.oval.def.protocols.TlsversionTest	\
+	scap.oval.def.protocols.SmtpTest	\
+	scap.oval.def.protocols.EntityStateTlsSignatureSchemeType	\
+	scap.oval.def.protocols.EntityObjectHttpMethodType	\
+	scap.oval.def.protocols.EntityStateTlsVersionType	\
+	scap.oval.def.protocols.HttpState	\
+	scap.oval.def.protocols.TlscertificateTest	\
+	scap.oval.def.protocols.EntityStatePublicKeyTypeType	\
+	scap.oval.def.protocols.TlsciphersuiteState	\
 	scap.oval.def.protocols.ObjectFactory	\
-	scap.oval.def.protocols.package-info
+	scap.oval.def.protocols.SmtpObject	\
+	scap.oval.def.protocols.PortObject	\
+	scap.oval.def.protocols.EntityStateHttpMethodType	\
+	scap.oval.def.protocols.FtpObject	\
+	scap.oval.def.protocols.FtpState	\
+	scap.oval.def.protocols.SshState	\
+	scap.oval.def.protocols.TlsversionState	\
+	scap.oval.def.protocols.PortState	\
+	scap.oval.def.protocols.TlsciphersuiteObject	\
+	scap.oval.def.protocols.PortTest	\
+	scap.oval.def.protocols.SmtpState	\
+	scap.oval.def.protocols.HttpBehaviors	\
+	scap.oval.def.protocols.HttpObject	\
+	scap.oval.def.protocols.TlscertificateState	\
+	scap.oval.def.protocols.SshObject	\
+	scap.oval.def.protocols.TlscertificateObject	\
+	scap.oval.def.protocols.EntityStateTlsCipherSuiteType	\
+	scap.oval.def.protocols.HttpTest	\
+	scap.oval.def.protocols.SshTest	\
+	scap.oval.def.protocols.package-info	\
+	scap.oval.def.protocols.EntityObjectTlsCipherSuiteType	\
+	scap.oval.def.protocols.EntityStateProtocolType
